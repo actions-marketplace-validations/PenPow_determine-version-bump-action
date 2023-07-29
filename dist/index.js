360124b01fa59a7ba314aa41ab112c9c03f42e4a
PenPow <josh@penpow.dev>
2023-07-29 18:26:40 +0100

    feat!: use input

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,14 @@
-import core, { toPlatformPath } from '@actions/core';
+import core from '@actions/core';
 import conventionalRecommendBump from 'conventional-recommended-bump';
-import { readFileSync } from 'fs';
 import semver from 'semver';
 try {
+    if (!core.getInput)
+        throw new Error("missing current version");
     conventionalRecommendBump({ preset: 'angular' }, (error, recommendation) => {
         if (error ?? !recommendation.releaseType)
             return core.setFailed(error ?? 'Could not determine version bump');
-        console.log(import.meta.url);
-        const currentVersion = readFileSync(toPlatformPath("../../package.json"), "utf-8");
-        const pkg = JSON.parse(currentVersion);
-        if (!pkg.version)
-            return core.setFailed("No version field in package.json");
         core.setOutput("versionBump", recommendation.releaseType);
-        core.setOutput("bumpedVersion", semver.inc(pkg.version, recommendation.releaseType));
+        core.setOutput("bumpedVersion", semver.inc(core.getInput("currentVersion"), recommendation.releaseType));
     });
 }
 catch (error) {
